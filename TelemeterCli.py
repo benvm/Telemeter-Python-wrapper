25a64bfa0fdfd9b112c84242b7138c89d68b783f
Steven Van Bael <steven.v.bael@gmail.com>
2010-08-14 12:00:50 +0200

    added dailyUsage info and cleaned all the output

--- TelemeterCli.py
+++ TelemeterCli.py
@@ -1,9 +1,20 @@
 from TelemeterService import TelemeterService, TelemeterUsageData
+import sys
 
-print "starting execution"
+
 service = TelemeterService("username", "password")
-print "fetching data"
+print "contacting telemeter service...\n"
 response = service.fetchData()
-print "fetch complete"
-print "limit: " + str(response.limit)
-print "total: " + str(response.totalUsage)
+print "monthly usage\n-------------"
+print "volume limit: " + str(response.limit) + " " + response.unit
+print "volume used:  " + str(response.totalUsage) + " " + response.unit
+print "volume left:  " + str(response.limit - response.totalUsage) + " " + response.unit
+print "last calculation: " + str(response.timestamp)
+print "validity period: " + str(response.dailyUsage[0].day) + " until " + str(response.dailyUsage[-1].day)
+
+print "\ndaily usage\n-----------"
+
+for day in response.dailyUsage:
+    print str(day.day) + " : " + str(day.usage) + " " + response.unit
+
+
