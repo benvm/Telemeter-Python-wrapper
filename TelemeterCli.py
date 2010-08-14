f9d2d0e093e96e0f0f7a66a3adf3b42970657b32
Steven Van Bael <steven.v.bael@gmail.com>
2010-08-14 15:31:26 +0200

    Added exception handling

--- TelemeterCli.py
+++ TelemeterCli.py
@@ -1,20 +1,31 @@
 from TelemeterService import TelemeterService, TelemeterUsageData
-import sys
+
+username = "username"
+password = "password"
+
+def handleResponse(response):
+    print "monthly usage\n-------------"
+    print "volume limit: " + str(response.limit) + " " + response.unit
+    print "volume used:  " + str(response.totalUsage) + " " + response.unit
+    print "volume left:  " + str(response.limit - response.totalUsage) + " " + response.unit
+    print "last calculation: " + str(response.timestamp)
+    print "validity period: " + str(response.dailyUsage[0].day) + " until " + str(response.dailyUsage[-1].day)
+
+    print "\ndaily usage\n-----------"
+
+    for day in response.dailyUsage:
+        print str(day.day) + " : " + str(day.usage) + " " + response.unit
 
 
-service = TelemeterService("username", "password")
-print "contacting telemeter service...\n"
-response = service.fetchData()
-print "monthly usage\n-------------"
-print "volume limit: " + str(response.limit) + " " + response.unit
-print "volume used:  " + str(response.totalUsage) + " " + response.unit
-print "volume left:  " + str(response.limit - response.totalUsage) + " " + response.unit
-print "last calculation: " + str(response.timestamp)
-print "validity period: " + str(response.dailyUsage[0].day) + " until " + str(response.dailyUsage[-1].day)
 
-print "\ndaily usage\n-----------"
+# script entrypoint
+try:
+    service = TelemeterService(username, password)
+    print "contacting telemeter service...\n"
+    response = service.fetchData()
+    handleResponse(response)
+except Exception as ex:
+    print "an error occured: ", ex
 
-for day in response.dailyUsage:
-    print str(day.day) + " : " + str(day.usage) + " " + response.unit
 
 
