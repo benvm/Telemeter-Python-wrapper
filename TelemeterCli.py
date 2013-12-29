0575a026a7c741d7b56de0d66fbf0bd06b4f615c
Ben Van Mechelen <me@benvm.be>
2013-12-29 21:38:35 +0100

    Added support for FUP type subscription

--- TelemeterCli.py
+++ TelemeterCli.py
@@ -1,31 +1,43 @@
-from TelemeterService import TelemeterService, TelemeterUsageData
+from TelemeterService import TelemeterService, TelemeterVolumeData, TelemeterFUPData
+import sys
 
 username = "username"
 password = "password"
 
 def handleResponse(response):
-    print "monthly usage\n-------------"
-    print "volume limit: " + str(response.limit) + " " + response.unit
-    print "volume used:  " + str(response.totalUsage) + " " + response.unit
-    print "volume left:  " + str(response.limit - response.totalUsage) + " " + response.unit
-    print "last calculation: " + str(response.timestamp)
-    print "validity period: " + str(response.dailyUsage[0].day) + " until " + str(response.dailyUsage[-1].day)
+    
+    if isinstance(response, TelemeterVolumeData):
+        print "monthly usage\n-------------"
+        print "volume limit: " + str(response.limit) + " " + response.unit
+        print "volume used:  " + str(response.totalUsage) + " " + response.unit
+        print "volume left:  " + str(response.limit - response.totalUsage) + " " + response.unit
+        print "last calculation: " + str(response.timestamp)
+        print "validity period: " + str(response.dailyUsage[0].day) + " until " + str(response.dailyUsage[-1].day)
 
-    print "\ndaily usage\n-----------"
+        print "\ndaily usage\n-----------"
 
-    for day in response.dailyUsage:
-        print str(day.day) + " : " + str(day.usage) + " " + response.unit
+        for day in response.dailyUsage:
+            print str(day.day) + " : " + str(day.usage) + " " + response.unit
+    
+    elif isinstance(response, TelemeterFUPData):
+        print "FUP\n---"
+        print "used: " + str(response.totalUsage) + " " + response.unit
+        print "minimum: " + str(response.minUsage) + " " + response.unit
+        print "maximum: " + str(response.maxUsage) + " " + response.unit
+        print "last update: " + str(response.lastUpdate)
+        print "status: " + response.status
 
-
-
+    else:
+        print "Error: No valid response retrieved"
+        
 # script entrypoint
+
+
 try:
     service = TelemeterService(username, password)
-    print "contacting telemeter service...\n"
     response = service.fetchData()
     handleResponse(response)
 except Exception as ex:
     print "an error occured: ", ex
-
 
 
--- TelemeterService.py
+++ TelemeterService.py
@@ -14,10 +14,12 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Telemeter Python wrapper.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Support for FUP type subscription: Copyright (C) 2013 Ben Van Mechelen benvm.be@gmail.com
 
 from datetime import date, datetime, time
 from xml.dom import minidom
-import dateutil.parser
+from datetime import datetime
 import urllib
 import urllib2
 import base64
@@ -29,21 +31,36 @@ class TelemeterDailyUsageData:
 
 
 # contains the full response data
-class TelemeterUsageData:
-    timestamp = datetime.now()
-    timestampExpired = datetime.now()
-    unit = "MB"
+class TelemeterVolumeData:
+    timestamp = None
+    timestampExpired = None
+    unit = None
     limit = 0
     totalUsage = 0
     dailyUsage = []
-
+    
+class TelemeterFUPData:
+    timestamp = None
+    expiryTimestamp = None
+    lastUpdate = None
+    unit = None
+    minUsage = 0
+    MaxUsage = 0
+    totalUsage = 0
+    periodFrom = None
+    periodTill = None
+    periodDay = 0
+    status = None
+    statusNL = None
+    statusFR = None
+    startGrootverbruiker = None
+    startVrijVerbruik = None
+        
 
 # actual service encapsulation class
 class TelemeterService:
     username = ""
     password = ""
-    usageData = TelemeterUsageData()
-
 
     def __init__(self, username, password):
         self.username = username
@@ -59,25 +76,82 @@ class TelemeterService:
         req = urllib2.Request(url, data, headers)
         response = urllib2.urlopen(req, data).read()
         return self.parseResponse(response)
+        
 
     def parseResponse(self, responsedata):
         # parse the xml
         xmldoc = minidom.parseString(responsedata)
-        self.usageData = TelemeterUsageData()
+        
+        # See if we received a fault
+        fault = xmldoc.getElementsByTagName("TelemeterFault")
+        if fault:
+            code = fault.getElementsByTagName("Code")[0].childNodes[0].data
+            description = fault.getElementsByTagName("Description")[0].childNodes[0].data
+            raise RuntimeError(code + ": " + description)
+        
+        ticket = xmldoc.getElementsByTagName("Ticket")
+        fup = xmldoc.getElementsByTagName("FUP")
+        volume = xmldoc.getElementsByTagName("Volume")
+        
+        
+        if fup:
+            # FUP subsription
+            fup = fup[0]
+            self.usageData = TelemeterFUPData()
+            
+            for key in {"Period", "Usage", "Status", "StatusDescription", "Info"}:
+                nl = fup.getElementsByTagName(key)
+                if key is "Period":
+                    #ignoring UTC offset for now.
+                    self.usageData.periodFrom = datetime.strptime(nl[0].getElementsByTagName("From")[0].childNodes[0].data[:-6], '%Y-%m-%d')
+                    self.usageData.periodTill = datetime.strptime(nl[0].getElementsByTagName("Till")[0].childNodes[0].data[:-6], '%Y-%m-%d')
+                    self.usageData.periodDay = int(nl[0].getElementsByTagName("CurrentDay")[0].childNodes[0].data)
+                if key is "Usage":
+                    self.usageData.totalUsage = float(nl[0].getElementsByTagName("TotalUsage")[0].childNodes[0].data)
+                    self.usageData.unit = nl[0].getElementsByTagName("Unit")[0].childNodes[0].data
+                    self.usageData.minUsage = float(nl[0].getElementsByTagName("MinUsageRemaining")[0].childNodes[0].data)
+                    self.usageData.maxUsage = float(nl[0].getElementsByTagName("MaxUsageRemaining")[0].childNodes[0].data)
+                    self.usageData.lastUpdate = datetime.strptime(nl[0].getElementsByTagName("LastUpdate")[0].childNodes[0].data[:-10], '%Y-%m-%dT%H:%M:%S')
+                if key is "Status":
+                    self.usageData.status = nl[0].childNodes[0].data
+                if key is "StatusDescription":
+                    self.usageData.statusNL = nl[0].getElementsByTagName("NL")[0].childNodes[0].data
+                    self.usageData.statusFR = nl[0].getElementsByTagName("FR")[0].childNodes[0].data
+                if key is "Info":
+                    if nl:
+                        self.usageData.startGrootverbruiker = datetime.strptime(nl[0].getElementsByTagName("StartGrootverbruiker")[0].childNodes[0].data[:-6], '%Y-%m-%d')
+                        self.usageData.startVrijVerbruik = datetime.strptime(nl[0].getElementsByTagName("StartVrijVerbruik")[0].childNodes[0].data[:-6], '%Y-%m-%d')
+            
 
-        # get simple fields
-        self.usageData.limit = int(xmldoc.getElementsByTagName("Limit")[0].childNodes[0].data)
-        self.usageData.totalUsage = int(xmldoc.getElementsByTagName("TotalUsage")[0].childNodes[0].data)
-        self.usageData.unit = xmldoc.getElementsByTagName("Unit")[0].childNodes[0].data
-        self.timestamp = dateutil.parser.parse(xmldoc.getElementsByTagName("Timestamp")[0].childNodes[0].data)
+        elif volume:
+            # Volume subscription
+            volume = volume[0]
+            self.usageData = TelemeterVolumeData()
 
-        # get daily usage data for 30 days
-        days = xmldoc.getElementsByTagName("DailyUsage")
-        for xmlDay in days:
-            usage = TelemeterDailyUsageData()
-            daystr = xmlDay.childNodes[0].childNodes[0].data
-            usage.day = date(int(daystr[0:4]),int(daystr[5:7]),int(daystr[8:10]))
-            usage.usage = xmlDay.childNodes[1].childNodes[0].data
-            self.usageData.dailyUsage.append(usage)
+            # get simple fields
+            self.usageData.limit = int(volume.getElementsByTagName("Limit")[0].childNodes[0].data)
+            self.usageData.totalUsage = int(volume.getElementsByTagName("TotalUsage")[0].childNodes[0].data)
+            self.usageData.unit = volume.getElementsByTagName("Unit")[0].childNodes[0].data
+            
 
+            # get daily usage data for 30 days
+            days = vol.getElementsByTagName("DailyUsage")
+            for xmlDay in days:    
+                usage = TelemeterDailyUsageData()
+                daystr = xmlDay.childNodes[0].childNodes[0].data
+                usage.day = date(int(daystr[0:4]),int(daystr[5:7]),int(daystr[8:10]))
+                usage.usage = xmlDay.childNodes[1].childNodes[0].data
+                self.usageData.dailyUsage.append(usage)
+
+
+        if ticket:
+            # Ticket
+            ticket = ticket[0]
+            self.usageData.timestamp = datetime.strptime(ticket.getElementsByTagName("Timestamp")[0].childNodes[0].data[:-10], '%Y-%m-%dT%H:%M:%S')
+            self.usageData.expiryTimestamp = datetime.strptime(ticket.getElementsByTagName("ExpiryTimestamp")[0].childNodes[0].data[:-10], '%Y-%m-%dT%H:%M:%S')
+            
+            
         return self.usageData
+        
+        
+        
